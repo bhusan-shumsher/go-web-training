b9f8c8c906668e13b3c28b2daf5584a5a582dfeb
bhusan <bhusan@bhusans-MacBook-Pro.local>
2023-03-23 16:08:01 +0545

    render html templates

--- main.go
+++ main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world")
-	})
+const port = ":8000"
 
-	_ = http.ListenAndServe(":8000", nil)
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Printf(fmt.Sprintf("Starting application on port %s", port))
+
+	_ = http.ListenAndServe(port, nil)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template", err)
+	}
 }
